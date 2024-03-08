cb73e44be3b08b5a1e377070bdb596ac2fb4952c
M@W <mehdicr7wahid@gmail.com>
2024-03-08 23:42:59 +0430

    first changes

--- app/(groupLayout)/Home/page.tsx
+++ app/(groupLayout)/Home/page.tsx
@@ -9,7 +9,6 @@ export default function Home() {
       <HeroBanner />
       <Trending />
       <Popular />
-      
       <TopRated />
     </main>
   );
--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        { protocol: "https", hostname: "image.tmdb.org", port: "", pathname: "**" },
+        { protocol: "https", hostname: "img.youtube.com", port: "", pathname: "**" },
+      ],
+    },
+  };
+  
+  module.exports = nextConfig;
+  
